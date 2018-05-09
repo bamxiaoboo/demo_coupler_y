23f1684eed56668f7dd63d2a925c3313c9ee4e3e
Sherry <2892285844@qq.com>
2018-05-09 16:03:12 +0800

    6 : licom register field instances

--- coupling_atm_model_mod.f90
+++ coupling_atm_model_mod.f90
@@ -34,18 +34,36 @@ contains
         decomp_id = CCPL_register_normal_parallel_decomp("decomp_licom_grid", grid_H2D_id, decomp_size, local_grid_cell_index(:,local_id+1), "allocate for licom grid")
     end subroutine register_grids_decomps
 
-    subroutine register_component_coupling_configuration(time_step, comp_id)
+    subroutine register_component_coupling_configuration(decomp_size, sst, shf, ssh, mld, &
+            time_step, comp_id)
 
         use CCPL_interface_mod
 
         implicit none
 
+        integer, intent(in)          :: decomp_size
+        real(kind=RKIND), intent(in) :: sst(decomp_size), shf(decomp_size), ssh(decomp_size), mld(decomp_size)
         integer, intent(inout)       :: comp_id
         character*1024               :: annotation
-        integer                      :: time_step
+        integer                      :: time_step, fields_id(5)
+        integer                      :: field_id_psl, field_id_ts, field_id_flds, field_id_fsds
+        integer                      :: field_id_sst, field_id_ssh, field_id_shf, field_id_mld
 
         !----------------register time step to C-Coupler2--------------------------------------
         call CCPL_set_normal_time_step(licom_comp_id, time_step, annotation="setting the time step for licom")
+        !----------------register field instances to C-Coupler2--------------------------------
+        allocate(psl(decomp_size))
+        allocate(ts(decomp_size))
+        allocate(flds(decomp_size))
+        allocate(fsds(decomp_size))
+        field_id_psl = CCPL_register_field_instance(psl(1:decomp_size), "psl", decomp_id, grid_h2d_id, 0, usage_tag=CCPL_TAG_CPL_REST, field_unit="Pa", annotation="register field instance of Sea level pressure") 
+        field_id_ts = CCPL_register_field_instance(ts(1:decomp_size), "ts", decomp_id, grid_h2d_id, 0, usage_tag=CCPL_TAG_CPL_REST, field_unit="K", annotation="register field instance of Surface temperature")
+        field_id_fsds = CCPL_register_field_instance(fsds(1:decomp_size), "fsds", decomp_id, grid_h2d_id, 0, usage_tag=CCPL_TAG_CPL_REST, field_unit="W/m2", annotation="register field instance of Short wave downward flux at surface")
+        field_id_flds  = CCPL_register_field_instance(flds(1:decomp_size), "flds", decomp_id, grid_h2d_id, 0, usage_tag=CCPL_TAG_CPL_REST, field_unit="W/m2", annotation="register field instance of Long wave downward flux at surface")
+        field_id_sst  = CCPL_register_field_instance(sst, "sst", decomp_id, grid_h2d_id, 0, usage_tag=CCPL_TAG_CPL_REST, field_unit="C", annotation="register field instance of Sea surface temperature")
+        field_id_shf  = CCPL_register_field_instance(shf, "shf", decomp_id, grid_h2d_id, 0, usage_tag=CCPL_TAG_CPL_REST, field_unit="W/m2", annotation="register field instance of Net surface heat flux")
+        field_id_ssh = CCPL_register_field_instance(ssh, "ssh", decomp_id, grid_h2d_id, 0, usage_tag=CCPL_TAG_CPL_REST, field_unit="m", annotation="register field instance of Sea surface height")
+        field_id_mld = CCPL_register_field_instance(mld, "mld", decomp_id, grid_h2d_id, 0, usage_tag=CCPL_TAG_CPL_REST, field_unit="m", annotation="register field instance of Mixed layer depth")
 
     end subroutine register_component_coupling_configuration
     
